4c8678ea1a014a22994894245069120381b69c6f
Meshari <alh3rbi@gmail.com>
2021-03-31 15:43:32 -0600

    typo

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -13,7 +13,7 @@ export const live = livelogo
 export const githubLink = 'https://github.com/FyerBall'
 export const resumeLink = 'google.com'
 
-export const aboutMe = `Hi, my name is Meshari and I have always been passionate about technology and Computer Science. When I was growing up, I spent the majority of my time studying and learning network and security. It started with building my own PC to support my working and testing environments during that time, I got familiar with a few programming languages such as C++, Python, and Java. However, in late 2015, I started my Front-End development journey, and it has become my new passion. Now, I only use JavaScript, mainly ReactJS, along with HTML and CSS. I would welcome the opportunity to work with you.`
+export const aboutMe = `Hi, my name is Meshari and I have always been passionate about technology and Computer Science. When I was growing up, I spent the majority of my time studying and learning network and security. It started with building my own PC to support my working and testing environments. During that time, I got familiar with a few programming languages such as C++, Python, and Java. However, in late 2015, I started my Front-End development journey, and it has become my new passion. Now, I only use JavaScript, mainly ReactJS, along with HTML and CSS. I would welcome the opportunity to work with you.`
 // API
 export const apiUrl = 'api/projects'
 
